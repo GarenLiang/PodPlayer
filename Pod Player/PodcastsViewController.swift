f43bbeb9656f859cfeb5ff6a0661e54d1300e9d4
GarenLiang <ideabreeze@gmail.com>
2017-05-06 21:55:42 -0700

    podcast

---
+++ Pod Player/PodcastsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PodcastsViewController.swift
+//  Pod Player
+//
+//  Created by GarenLiang on 2017/5/6.
+//  Copyright © 2017年 GarenLiang. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastsViewController: NSViewController {
+
+    @IBOutlet weak var podcastURLTextField: NSTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    @IBAction func addPodcastClicked(_ sender: Any) {
+        
+    }
+}
